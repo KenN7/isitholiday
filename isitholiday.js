2881eb1fba0d66dbd1be41fe7ea6d353e8cb5e7c
Ken <ken@kenh.fr>
2020-05-20 14:54:53 +0200

    update for covid holidays

--- isitholiday.js
+++ isitholiday.js
@@ -28,6 +28,8 @@ const holidays = [
     ["2020-04-13", "Easter."],
     ["2020-05-01", "the celebration of work."],
     ["2020-05-21", "the feast of the Ascension."],
+    ["2020-05-22", "the first covid holiday."],
+    ["2020-05-29", "the second covid holiday."],
     ["2020-06-01", "the Pentecost."],
     ["2020-07-21", "the Belgian national day."],
     ["2020-11-02", "a random holiday."],
@@ -48,17 +50,17 @@ class Checker {
 
     dateEquality(date1, date2) {
         return (date1.getFullYear() === date2.getFullYear()) &&
-           // getMonth is 0-indexed
-           (date1.getMonth() === date2.getMonth()) &&
-           (date1.getDate() === date2.getDate())
+            // getMonth is 0-indexed
+            (date1.getMonth() === date2.getMonth()) &&
+            (date1.getDate() === date2.getDate())
     }
 
     check(offset) {
         let day = new Date()
         day.setDate(day.getDate() + offset)
         //console.log(day)
-        let holiday = this.days.find( x => this.dateEquality( new Date(x[0]+" 12:00:00Z"), day ) )
-        if ( holiday ) { //if special holiday from list
+        let holiday = this.days.find(x => this.dateEquality(new Date(x[0] + " 12:00:00Z"), day))
+        if (holiday) { //if special holiday from list
             return holiday
         } else { //maybe its friday or saturday ?
             return this.checkWE(day)
@@ -69,8 +71,8 @@ class Checker {
         let day = new Date()
         //console.log(day)
         day.setDate(day.getDate() + 1)
-        let nextholiday = this.days.find( x => new Date(x[0]+" 00:00:00Z") > day )
-        if ( nextholiday ) { //if special holiday from list
+        let nextholiday = this.days.find(x => new Date(x[0] + " 00:00:00Z") > day)
+        if (nextholiday) { //if special holiday from list
             // console.log(nextholiday)
             return nextholiday
         } else {
@@ -91,32 +93,24 @@ class Checker {
 
     render() {
         let tomo = this.check(1)
-        let reasonBox = ""
-        if ( tomo ) {
-            reasonBox = "because "+tomo[0]+" is "+tomo[1]
-        }
         let today = this.check(0)
-        let reasontdBox = ""
-        if ( today ) {
-            reasontdBox = "because "+today[0]+" is "+today[1]
-        }
-        return(`
+        return (`
         <h1>Is it holiday tomorrow at ULB?</h1>
         <div id="holiday">${ tomo ? 'YES.' : 'NO.' }</div>
-        <div id="why">${reasonBox}</div>
+        <div id="why">${tomo ? `because ${tomo[0]} is ${tomo[1]}` : ''}</div>
           <!-- page today -->
         <div id="today">
         <hr>
         <h4>and what about today?</h4>
         <div id="holidaytd">${ today ? 'YES.' : 'NO.' }</div>
-        <div>${reasontdBox}</div>
+        <div>${today ? `because ${today[0]} is ${today[1]}` : ''}</div>
         <h6>By the way, the next one will be on ${this.nextHoliday()[0]} </h6>
         </div>`)
     }
 
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     const check = new Checker(holidays)
     document.getElementById("app").innerHTML = check.render()
 })
